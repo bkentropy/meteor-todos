c2cdb103b75d0a8dd5a5ea97fd3129a363520a17
Brian Kustra <bkustrakb@gmail.com>
2016-04-23 15:07:53 -0500

    Making progress on todo app, includes login now

---
+++ imports/ui/AccountsUIWrapper.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+export default class AccountsUIWrapper extends Component {
+  componentDidMount() {
+    // Use Meteror Blaze to render login buttons
+    this.view = Blaze.render(Template.loginButtons, ReactDOM.findDOMNode(this.refs.container));
+  }
+
+  componentWillUnmout() {
+    // Clean up Blaze view
+    Blaze.remove(this.view);
+  }
+
+  render() {
+    // Just render a placeholder container that will be fill in 
+    return <span ref="container"/>
+  }
+
+}
+
+
+
+
+
